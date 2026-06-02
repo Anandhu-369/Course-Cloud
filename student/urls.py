df1625ba54f31963bd765e76866bef3f5a87dbfe
Anandhu A <anandhupc333@gmail.com>
2026-06-02 12:59:26 +0530

    'addtocart'

--- student/urls.py
+++ student/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('signup',SignupView.as_view(),name='signup'),
     path('studenthome',StudentHomeView.as_view(),name='shome'),
     path('course_details/<int:cid>',CourseDetailsView.as_view(),name="course_details"),
+    path('addtocart/<int:cid>',AddToCartView.as_view(),name='addtocart'),
 ]
